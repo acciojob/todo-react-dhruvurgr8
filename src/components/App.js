084a305ca2b9f19a374de42689cb8ae31ce5feb7
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-14 05:27:19 +0000

    solution added

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,51 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [tasks, setTask] = useState([]);
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
+  };
+  const handleClick = () => {
+    setTask((preTask) => {
+      return [...preTask, inputValue];
+    });
+  };
+
+  const handleDelete = (taskToDelete) => {
+    const newTasks = tasks.filter((task) => {
+      return task !== taskToDelete;
+    });
+    setTask([...newTasks]);
+  };
   return (
-    <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
+    <>
+      <h1>To-Do List</h1>
+      <input value={inputValue} onChange={handleChange} type="text" />
+      <button onClick={handleClick}>ADD TODO</button>
+      <div className="task-list">
+        {tasks.map((task, index) => (
+          <div className="list">
+            <ul key={index}>
+              <li>
+                <h2>{task}</h2>
+              </li>
+              <li>
+                <button
+                  onClick={() => {
+                    handleDelete(task);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default App
